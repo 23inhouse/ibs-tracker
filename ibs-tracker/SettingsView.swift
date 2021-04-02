ecb5e2c928e2e83b2a1f6069980c48729d599897
Benjamin Lewis <23inhouse@gmail.com>
2021-04-02 15:12:29 +0200

    Update the JSON data import url

--- ibs-tracker/SettingsView.swift
+++ ibs-tracker/SettingsView.swift
@@ -11,7 +11,7 @@ struct SettingsView: View {
   @Environment(\.presentationMode) private var presentation
   @EnvironmentObject private var appState: IBSData
 
-  @State private var url = "https://gist.githubusercontent.com/23inhouse/d66aeb52ce44fdf61ab7f36f89509ec3/raw/8272d7dafcbeaf8b427d8b8b7ad7e290c0ff9967/import.json"
+  @State private var url = "https://gist.githubusercontent.com/23inhouse/d66aeb52ce44fdf61ab7f36f89509ec3/raw/ca224febae39a08ddb28f6e43143841ee0d7e505/import.json"
   @State private var truncate = false
   @State private var includeFood = false
 
