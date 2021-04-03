dcf890d054996bcd8aba02642f8be000b522637a
Benjamin Lewis <23inhouse@gmail.com>
2021-04-04 00:39:51 +0200

    UX enable action grid scrolling to reach the buttons

--- ibs-tracker/ActionGridView.swift
+++ ibs-tracker/ActionGridView.swift
@@ -10,37 +10,72 @@ import SwiftUI
 struct ActionGridView: View {
   var body: some View {
     NavigationView {
-      VStack(alignment: .center) {
-        Spacer()
-        AppIconView()
-          .foregroundColor(.secondary)
-        Spacer()
-        PaddedHStack {
-          ActionNavigationLink(type: .note, text: "Notes") { NoteFormView() }
-          ActionNavigationLink(type: .medication, text: "medicaiton") { MedicationFormView() }
-          ActionNavigationLink(type: .food, text: "Food") { FoodFormView() }
+      ScrollViewReader { scroller in
+        ScrollView {
+          appIconButton(scroller: scroller, anchor: .bottom, rotate: .degrees(180))
+          Spacer()
+            .height(400)
+          HStack {
+            Spacer()
+            VStack(alignment: .center) {
+              appIconButton(scroller: scroller, anchor: .top)
+              contentGrid
+                .id(1)
+              appIconButton(scroller: scroller, anchor: .bottom, rotate: .degrees(180))
+            }
+            Spacer()
+          }
+          Spacer()
+            .height(400)
+          appIconButton(scroller: scroller, anchor: .top)
         }
-
-        PaddedHStack {
-          ActionNavigationLink(type: .mood, text: "Mood\nStress") { MoodFormView() }
-          ActionNavigationLink(type: .ache, text: "Headache\nBody ache") { AcheFormView() }
-          ActionNavigationLink(type: .gut, text: "Bloating\nPain") { GutFormView() }
-        }
-
-        PaddedHStack {
-          ActionNavigationLink(type: .skin, text: "Skin") { SkinFormView() }
-          ActionNavigationLink(type: .weight, text: "Weight") { WeightFormView() }
-          ActionNavigationLink(type: .bm, text: "Poop") { BMFormView() }
+        .onAppear {
+          scroller.scrollTo(id: 1, anchor: .bottom)
         }
       }
-      .padding(.bottom, 20)
       .navigationBarTitleDisplayMode(.inline)
       .toolbar {
         ToolbarItem(placement: .principal) {
-          Text("Add record")
+          Text("IBS Tracker")
         }
       }
     }
+  }
+
+  private var contentGrid: some View {
+    VStack(alignment: .center) {
+      PaddedHStack {
+        ActionNavigationLink(type: .note, text: "Notes") { NoteFormView() }
+        ActionNavigationLink(type: .medication, text: "medicaiton") { MedicationFormView() }
+        ActionNavigationLink(type: .food, text: "Food") { FoodFormView() }
+      }
+
+      PaddedHStack {
+        ActionNavigationLink(type: .mood, text: "Mood\nStress") { MoodFormView() }
+        ActionNavigationLink(type: .ache, text: "Headache\nBody ache") { AcheFormView() }
+        ActionNavigationLink(type: .gut, text: "Bloating\nPain") { GutFormView() }
+      }
+
+      PaddedHStack {
+        ActionNavigationLink(type: .skin, text: "Skin") { SkinFormView() }
+        ActionNavigationLink(type: .weight, text: "Weight") { WeightFormView() }
+        ActionNavigationLink(type: .bm, text: "Poop") { BMFormView() }
+      }
+    }
+    .padding(.vertical, 20)
+    .frame(maxWidth: 380)
+  }
+
+  private func appIconButton(scroller: ScrollViewProxy, anchor: UnitPoint, rotate angle: Angle = .degrees(0)) -> some View {
+    AppIconView()
+      .contentShape(Rectangle())
+      .rotate(angle)
+      .foregroundColor(.secondary)
+      .onTapGesture {
+        withAnimation {
+          scroller.scrollTo(id: 1, anchor: anchor)
+        }
+      }
   }
 }
 
