861ff69dae1858e547cc07906ebbbfae1efad57f
Benjamin Lewis <23inhouse@gmail.com>
2021-04-07 13:40:03 +0200

    UI improve tag word wrapping

--- ibs-tracker/Components/TagCloudView.swift
+++ ibs-tracker/Components/TagCloudView.swift
@@ -19,15 +19,22 @@ struct TagCloudView: View {
   var resize: Bool = false
 
   var font: Font {
-    guard resize == true else { return .footnote }
+    guard resize else { return .footnote }
     return fonts[tags.count] ?? .caption2
   }
 
+  var unwrappableTags: [String] {
+    tags.map { tag in
+      String(tag.replacingOccurrences(of: " ", with: "\u{00a0}"))
+    }
+  }
+
   var body: some View {
-    Text(tags.map { $0.firstUppercased }.joined(separator: ", "))
-      .font(font)
-      .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+    Text(unwrappableTags.map { $0.firstUppercased }.joined(separator: ", "))
       .align(.trailing)
+      .frame(maxWidth: .infinity, alignment: .trailing)
+      .font(font)
+      .foregroundColor(.blue)
   }
 }
 
@@ -35,7 +42,7 @@ struct TagCloudView_Previews: PreviewProvider {
   static var previews: some View {
     List {
       TagCloudView(tags: ["tow words", "apple", "google", "amazon", "Microsoft"])
-      TagCloudView(tags: ["tow words", "apple", "google", "amazon", "Microsoft", "Oracle", "Facebook", "full evacuation", "Mr whippy", "Sweet smelling", "Good color", "Best so far", "medium pressure", "high pressure", "Tags need a maximum length"])
+      TagCloudView(tags: ["full evacuation", "Mr whippy", "Sweet smelling", "Good color", "Best so far", "medium pressure", "high pressure", "Tags need a maximum length"])
       TagCloudView(tags: ["one tag"], resize: true)
       TagCloudView(tags: ["tow words", "apple"], resize: true)
       TagCloudView(tags: ["more words", "apple", "google"], resize: true)
